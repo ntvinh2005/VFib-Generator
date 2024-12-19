e62a79631debb025992cf222657c3cb7d01bc6ee
Vinh Nguyen <ntvinhgv@gmail.com>
2024-12-19 12:35:59 -0500

    Add sinus look to the VFib waveform

--- augmented_waveform.js
+++ augmented_waveform.js
@@ -9,6 +9,8 @@ const ctx = canvas.getContext('2d');
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 
+let prevYValue = 0;
+
 // Animation variables
 let offset = 0; // This variable is use to track the horizontal scrolling
 
@@ -49,32 +51,52 @@ function updateNoise() {
     }
 }
 
-const numberOfSection = 5;
+let startEndIndexArray = [];
+
+const numberOfSection = 3;
 const sectionWidth = (dataArray.length - dataArray.length % numberOfSection) / numberOfSection;
+let parabolaConstant = 0.05;
 function updateSectionRandomOffsets() {
     let sectionRandomOffset = 0;
+    startEndIndexArray = [];
 
     for (let sectionIndex = 0; sectionIndex < numberOfSection; sectionIndex++) {
         const sectionStart = sectionIndex * sectionWidth;
         const sectionEnd = sectionIndex === numberOfSection - 1
             ? dataArray.length //In case the last section do not have enough number of elements equal to sectionWidth.
             : sectionStart + sectionWidth;
+        startEndIndexArray.push(sectionStart, sectionEnd);
 
         if (sectionIndex % 2 === 0) sectionRandomOffset = getRandomValue(0, 1000);
         else if (sectionIndex % 2 === 1) sectionRandomOffset = getRandomValue(-1000, 0);
 
         // We apply the random offset to all indices in this section
-        for (let i = sectionStart; i < sectionEnd; i++) {
-            randomSectionOffsets[i] = sectionRandomOffset;
+        if (sectionRandomOffset > 0) {
+            for (let i = sectionStart; i < sectionEnd; i++) {
+                randomSectionOffsets[i] = (sectionWidth/2)*(sectionWidth/2)*parabolaConstant - parabolaConstant * (i - sectionStart - sectionWidth/2) * (i - sectionStart - sectionWidth/2);
+            }
+        }
+        else {
+            for (let i = sectionStart; i < sectionEnd; i++) {
+                randomSectionOffsets[i] = -(sectionWidth/2)*(sectionWidth/2)*parabolaConstant + parabolaConstant * (i - sectionStart - sectionWidth/2) * (i - sectionStart - sectionWidth/2);
+            }
         }
     }
 }
 
+function drawRedLine(x) {
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, canvasHeight);
+    ctx.stroke();
+}
 
 function drawECG() {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    // This is to create grid line
+    // This is to create grid lines
     ctx.strokeStyle = "#ddd";
     ctx.lineWidth = 1;
     for (let x = 0; x < canvasWidth; x += 50) {
@@ -90,16 +112,22 @@ function drawECG() {
         ctx.stroke();
     }
 
+    //draw red line for start and end of a section
+    for (let i = 0; i < startEndIndexArray.length; i++) {
+        let x = startEndIndexArray[i] * timeScale - offset;
+        drawRedLine(x);
+    }
+
+    // Draw the ECG waveform
     ctx.strokeStyle = "blue";
     ctx.lineWidth = 2;
     ctx.beginPath();
-    let startY = canvasHeight / 2;
-    //End of grid line code.
+    const startY = canvasHeight / 2;
 
-    //This is where we start drawing our ECG waveform.
     for (let x = 0; x <= canvasWidth; x++) {
-        let dataIndex = Math.floor((x + offset) / timeScale) % dataArray.length;
-        let yValue = startY - (noiseArray[dataIndex] +  randomOffsets[dataIndex] + randomSectionOffsets[dataIndex]) * amplitudeScale;
+        const dataIndex = Math.floor((x + offset) / timeScale) % dataArray.length;
+        const yValue = startY - (noiseArray[dataIndex] + randomOffsets[dataIndex] + randomSectionOffsets[dataIndex]) * amplitudeScale;
+
         if (yValue >= startY - 1000 * amplitudeScale) {
             if (x === 0) {
                 ctx.moveTo(x, yValue);
@@ -113,10 +141,8 @@ function drawECG() {
 
     offset = (offset + 1) % (dataArray.length * timeScale);
     if (offset === 1) {
-        updateRandomOffsets();
         updateSectionRandomOffsets();
-        updateNoise();
-        //console.log(randomSectionOffsets);
+        console.log(randomSectionOffsets);
     }
 
     requestAnimationFrame(drawECG);
